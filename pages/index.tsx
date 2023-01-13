23f62ebf522f17f05a2004eb7f39e2a89d1ee98a
Andrew <anaguchidiebere@gmail.com>
2023-01-13 16:40:15 +0100

    fixed nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <div className="container mx-auto px-10 mb-8 ">
+    <div className="container mx-auto px-10 ">
       <Head>
         <title>Chidi's Blog</title>
         <link rel="icon" href="/bg.jpg" />
