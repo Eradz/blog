7ccc5841aa504b5e792df60f38739edf56707ba4
Andrew <anaguchidiebere@gmail.com>
2023-01-13 15:59:23 +0100

    mobile nav done

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,13 +8,14 @@ import {IoChevronForwardSharp} from 'react-icons/io5'
 
 const Nav = () => {
     const categories = ['Charts', 'Music', 'Culture', 'Media', 'Business','Pro Tools', 'Billboard Espanol', 'Honda Music']
+    const footer = ['About us', "Accessibility", "Advertise", "Shop", "Events", 'Careers', "Contact Us"]
   return (
-  <div className='text-white'>
+  <div className='text-white box-border'>
         <div className='flex bg-blue items-center px-4 py-3 gap-4 '>
-            <ImSearch size={25} className=''/>
-            <input type='search' placeholder='Search' name='search' className='bg-blue focus:outline-none '/>
+            <ImSearch size={25} className='w-[8%]'/>
+            <input type='search' placeholder='Search' name='search' className='bg-blue focus:outline-none w-[92%] '/>
         </div>
-      <div className='bg-black'>
+      <div className='bg-black h-[100vh]'>
         <div className=' py-8 px-4 flex justify-center gap-9 border-b-[0.5px] border-white'>
             <div className='flex w-[90%]  gap-[10%]'>
               <button className='py-2 w-[50%] outline outline-white outline-1 hover:cursor-pointer bg-cyan-500'>SUBSCRIBE</button>
@@ -39,13 +40,20 @@ const Nav = () => {
           </span>
           <div className='flex flex-col items-center   w-[60%] mx-auto'>
             <div className='flex w-full  bg-blue p-2'>
-            <input name='email' placeholder='Enter your Email' className='bg-blue focus:outline-none border-b-[1px] border-b-terms w-[90%]'/>
-            <IoChevronForwardSharp size={30} className='w-[10%]'/>
+            <input name='email' placeholder='Enter your Email' className='bg-blue focus:outline-none border-b-[1px] border-b-terms w-[92%]'/>
+            <IoChevronForwardSharp size={30} className='w-[8%]'/>
             </div>
             <p className='text-[12px] w-full text-terms'>
             This site is protected by reCAPTCHA Enterprise and the Google <u className='hover:cursor-pointer'>Privacy Policy</u> and <u className='hover:cursor-pointer'>Terms of Service</u> apply.
             </p>
           </div>
+      <div className='flex text-center gap-2 w-full mx-auto p-2'>
+      {footer.map((link) =>{
+        return(
+          <p className='text-[12px]'>{link}</p>
+        )
+      })}
+      </div>
       </div>
     </div>
   </div>
